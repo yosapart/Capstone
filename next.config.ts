47f844e3aa72adbbe85d3f4c7a82af15c092d010
IKeeN-0 <hengkung20@gmail.com>
2026-06-03 15:11:54 +0700

    allow ngrok

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['eloquence-wick-grumbly.ngrok-free.dev'],
 };
 
 export default nextConfig;
